bcfbf86ed12a1bc2e1039ca0dd246fc8552d5cec
allan <allan.almeida@ufba.br>
2025-02-27 21:31:26 -0300

    update esp code to use OSC instead of MIDI

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,37 @@
-#include <Arduino.h>
-
-#define MIDI_CHANNEL_ON 0x90
-#define MIDI_CHANNEL_OFF 0x80
-#define MIDI_C4 60
-#define MIDI_VELOCITY_MAX 127
-#define MIDI_VELOCITY_MIN 0
 #define SWITCH_PIN 5
 
-const long DEBOUNCE_DELAY = 100; // ms
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+// Wifi settings
+const char* ssid = "Ap 201";
+const char* password = "pzT6kozQ!g$G4!SG";
+
+// OSC settings
+const char* host = "192.168.100.16";
+const int port = 8000;
+WiFiUDP Udp;
+
+const long DEBOUNCE_DELAY = 200; // ms
 long lastDebounceTime = 0;
 bool lastSwitchState = LOW;
 
-/**
- * @brief Send a MIDI note on message
- * 
- * @param note Note to play
- * @param velocity Velocity of the note
- */
-void sendMIDINoteOn(int note, int velocity);
-
-/**
- * @brief Send a MIDI note off message
- * 
- * @param note Note to stop playing
- * @param velocity Velocity of the note
- */
-void sendMIDINoteOff(int note, int velocity);
-
+void sendOSCMessage(const char* path, int value);
 
 void setup() {
-  // Setup serial for MIDI
-  Serial.begin(115200);
+    pinMode(SWITCH_PIN, INPUT_PULLUP);
 
-  // Setup switch pin
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("Connected");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -43,9 +41,8 @@ void loop() {
   // If the switch state has been stable for a while
   if(switchState == HIGH && lastSwitchState == LOW) {
     if((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-      sendMIDINoteOn(MIDI_C4, MIDI_VELOCITY_MAX);
-      delay(200);
-      sendMIDINoteOff(MIDI_C4, MIDI_VELOCITY_MIN);
+      sendOSCMessage("/track/1/solo/toggle", 1);
+      Serial.println("Sent OSC message to toggle solo on track 1");
       lastDebounceTime = millis();
     }
   }
@@ -53,14 +50,29 @@ void loop() {
   lastSwitchState = switchState;
 }
 
-void sendMIDINoteOn(int note, int velocity) {
-  Serial.write(MIDI_CHANNEL_ON);
-  Serial.write(note);
-  Serial.write(velocity);
-}
+void sendOSCMessage(const char* path, int value) {
+  char buffer[64];  // Adjust size as needed
+  int index = 0;
 
-void sendMIDINoteOff(int note, int velocity) {
-  Serial.write(MIDI_CHANNEL_OFF);
-  Serial.write(note);
-  Serial.write(velocity);
+  // Add the OSC address
+  index += sprintf(&buffer[index], "%s", path);
+  index++;  // Null terminator
+  while (index % 4 != 0) buffer[index++] = '\0';  // Pad to 4-byte boundary
+
+  // Add the type tag
+  buffer[index++] = ',';
+  buffer[index++] = 'i';
+  buffer[index++] = '\0';
+  buffer[index++] = '\0';
+
+  // Add the integer argument (32-bit big-endian)
+  buffer[index++] = (value >> 24) & 0xFF;
+  buffer[index++] = (value >> 16) & 0xFF;
+  buffer[index++] = (value >> 8) & 0xFF;
+  buffer[index++] = value & 0xFF;
+
+  // Send the packet
+  Udp.beginPacket(host, port);
+  Udp.write((uint8_t*)buffer, index);
+  Udp.endPacket();
 }
